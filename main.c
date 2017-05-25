a1278fdd60d5642ac5bf29a29b567b28e9e371e1
douglasmamuty <douglas.shusano@gmail.com>
2017-05-25 20:37:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,80 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+
+
 struct Alunos{
 	char nome[30];
 	int matricula;
-	int codDisciplina[3];
+	int codDisciplina;
 };
 typedef struct Alunos aluno;
- 
+
 struct Disciplina{
 	char nome[30];
 	int codDisciplina;
 	int codTurma;
 };
 typedef struct Disciplina disciplina;
- 
+
 struct Curso{
 	char nome[30];
 	int codCurso;
 };
 typedef struct Curso curso;
- 
+
 struct Turma{
 	char nome[30];
 	int codTurma;
 	int codCurso;
 };
 typedef struct Turma turma;
- 
+/*
 void fnCadastraCurso(){
- 
+
 }
- 
+
 void fnCadastraTurma(){
- 
+
 }
- 
+
 void fnCadastraDisciplina(){
- 
+
 }
- 
+
 void fnCadastraAluno(){
- 
+
 }
- 
+
 void fnListaAluno(int matricula){
- 
+
 }
- 
+
 void fnListaCurso(){
- 
+
 }
- 
+
 void fnListaTurma(int codCurso){
- 
+
 }
- 
+
 void fnListaDisciplina(int codTurma){
- 
-}
-*/
+
+}*/
+
 void main(){
-	//Declaração de variaveis dinamicas com malloc()
-  /*  disciplina *disciplinas = (disciplina *) malloc(sizeof(disciplina));
+	/*//Declaração de variaveis dinamicas com malloc()
+    disciplina *disciplinas = (disciplina *) malloc(sizeof(disciplina));
 	aluno *alunos = (aluno *) malloc(sizeof(aluno));
 	turma *turmas = (turma *) malloc(sizeof(turma));
 	curso *cursos = (curso *) malloc(sizeof(curso));*/
- 
-	//Menu do sistema
+
+	/*struct rec *ptr_one;*/
+    /*ptr_one =(struct rec *) malloc (sizeof(struct rec));*/
+    aluno *alunos = (aluno *) malloc(sizeof(aluno));
+
+    alunos->nome[0] = 'D';
+    alunos->nome[1] = 'o';
+    alunos->nome[2] = 'u';
+    alunos->nome[3] = 'g';
+    alunos->nome[4] = 'l';
+    alunos->nome[5] = 'a';
+    alunos->nome[5] = 's';
+    alunos->matricula = 35514;
+    alunos->codDisciplina = 10;
+
+    printf("First value: %c\n", alunos->nome);
+    memcpy(string, array, 5);
+    string[x] = '\0'; 
+    printf("Matricula: %d\n", alunos->matricula);
+    printf("Codigo Disciplina: %d\n", alunos->codDisciplina);
+
+    free(alunos);
+
+	/*//Menu do sistema*/
 	int controle = 9999,entrada,controle1 = 9999,entrada1;
 	while(controle != 0){
     	printf("Selecione uma opcao do menu:\n");
     	printf("1 - Curso\n2 - Turma\n3 - Disciplina\n4 - Aluno\n0 - Exit\n");
     	controle1 = 9999;
     	scanf("%d", &entrada);
- 
+
     	switch(entrada){
         	case 1:
             	while(controle1 != 0){
@@ -83,10 +106,10 @@ void main(){
                 	scanf("%i", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                        	//fnCadastraCurso();
+                        	/*fnCadastraCurso();*/
                         	break;
                     	case 2:
-                        	//fnListaCurso();
+                        	/*fnListaCurso();*/
                         	break;
                     	case 0:
                         	controle1 = 0;
@@ -104,10 +127,10 @@ void main(){
                 	scanf("%d", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                        	//fnCadastraTurma();
+                        	/*fnCadastraTurma();*/
                         	break;
                     	case 2:
-                        	//fnListaTurma(0);
+                        	/*fnListaTurma(0);*/
                         	break;
                     	case 0:
                         	controle1 = 0;
@@ -125,10 +148,10 @@ void main(){
                 	scanf("%d", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                        	//fnCadastraDisciplina();
+                        	/*fnCadastraDisciplina();*/
                         	break;
                     	case 2:
-                        	//fnListaDisciplina(0);
+                        	/*fnListaDisciplina(0);*/
                         	break;
                     	case 0:
                         	controle1 = 0;
@@ -146,10 +169,10 @@ void main(){
                 	scanf("%d", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                       	// fnCadastraAluno();
+                       	/* fnCadastraAluno();*/
                         	break;
                     	case 2:
-                        	//fnListaAluno(0);
+                        	/*fnListaAluno(0);*/
                         	break;
                     	case 0:
                         	controle1 = 0;
@@ -169,13 +192,12 @@ void main(){
                 	break;
     	}
 	}
- 
+
 }
   /* CRIA ARQUIVO E SALVA
-  
+
   FILE *fp;
    int c;
-
    fp = fopen("C:\\Users\\Aluno\\Desktop\\arquivoTex.txt","r");
    while(1)
    {
@@ -187,14 +209,9 @@ void main(){
       printf("%c", c);
    }
    fclose(fp);
-
-
   FILE * fp;
-
   fp = fopen ("C:\\Users\\Aluno\\Desktop\\arquivoTex.txt", "w+");
   fprintf(fp, "%s %s %s %d", "We", "are", "in", 2012);
-
   fclose(fp);
-
   return(0);
 */
