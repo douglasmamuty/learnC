3c0242a7469edb2ba06757b64913c73a4d12ea9d
douglasmamuty <douglas.shusano@gmail.com>
2017-06-06 21:10:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,93 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define Tamanho 100
+struct Alunos{
+	char nome[30];
+	int matricula;
+	int codDisciplina;
+};
+typedef struct Alunos aluno;
+
+struct Disciplina{
+	char nome[30];
+	int codDisciplina;
+	int codTurma;
+};
+typedef struct Disciplina disciplina;
+
+struct Curso{
+	char nome[30];
+	int codCurso;
+};
+typedef struct Curso curso;
+
+struct Turma{
+	char nome[30];
+	int codTurma;
+	int codCurso;
+};
+typedef struct Turma turma;
+
+int fnCadastraCurso(curso *cursos, int intContador){
+    intContador++;
+    printf("Digite o nome do curso: \n");
+    scanf("%s",cursos[intContador].nome);
+    printf("Digite o codigo do curso: \n");
+    scanf("%d", &cursos[intContador].codCurso);
+
+    return intContador;
+}
+
+void fnListaCurso(curso *cursos,int intContador){
+    printf(" ______________________________________________\n");
+    printf("|Codigo               | Curso                  |\n");
+    printf(" ______________________________________________\n");
+    for(int i = 1; i <= intContador ;i++){
+            printf("|%s               | %d                  |\n",cursos[i].nome,cursos[i].codCurso);
+    }
+    printf(" ______________________________________________\n");
+}
+
+int fnCadastraTurma(turma *turmas,int intContador){
+    intContador++;
+    printf("Digite o nome da turma: \n");
+    scanf("%s",turmas[intContador].nome);
+    printf("Digite o codigo da turma: \n");
+    scanf("%d", &turmas[intContador].codTurma);
+    printf("Digite o codigo do curso que a turma pertence: \n");
+    scanf("%d", &turmas[intContador].codCurso);
+
+    return intContador;
+}
+const char* fnSearchCurso(int codCurso,curso *cursos){
+    for(int i = 1; i <= Tamanho ;i++){
+            if(cursos[i].codCurso == codCurso){
+                return cursos[i].nome;
+            }
+    }
+    return "Nao foi possivel encontrar o Curso";
+}
+
+void fnListaTurma(int codCurso,turma *turmas,int intContador,curso *cursos){
+
+    printf("Curso : %s\n",fnSearchCurso(codCurso,cursos));
+    printf(" ______________________________________________\n");
+    printf("|Codigo               | Turma                  |\n");
+    printf(" ______________________________________________\n");
+    for(int i = 1; i <= intContador ;i++){
+            printf("|%d               | %s                  |\n",turmas[i].codTurma, turmas[i].nome);
+    }
+    printf(" ______________________________________________\n");
+}
+/*
+void fnCadastraDisciplina(){
+}
+
+}#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct Alunos{
 	char nome[30];
 	int matricula;
@@ -31,15 +118,19 @@ typedef struct Turma turma;
 
 int fnCadastraCurso(curso *cursos){
     printf("Digite o nome do curso: \n");
-    scanf("%s",cursos[0].nome);
+    scanf("%s",cursos[0].nome);int fnCadastraCurso(curso *cursos, int intContador){
+    intContador++;
+    printf("Digite o nome do curso: \n");
+    scanf("%s",cursos[intContador].nome);
+    printf("Digite o codigo do curso: \n");
+    scanf("%d", &cursos[intContador].codCurso);
+
+    return intContador;
+}
     printf("Digite o codigo do curso: \n");
     scanf("%d", &cursos[0].codCurso);
 
     return 1;
-}
-/*
-void fnCadastraTurma(){
-
 }
 
 void fnCadastraDisciplina(){
@@ -54,27 +145,21 @@ void fnListaAluno(int matricula){
 
 }
 
-void fnListaCurso(){
-
-}
-
-void fnListaTurma(int codCurso){
-
-}
-
 void fnListaDisciplina(int codTurma){
 
 }*/
 
 void main(){
 	/*Declaração de variaveis dinamicas com malloc()*/
-    disciplina *disciplinas = (disciplina *) malloc(sizeof(disciplina)* 10);
-	aluno *alunos = (aluno *) malloc(sizeof(aluno)*30);
-	turma *turmas = (turma *) malloc(sizeof(turma)*6);
-	curso *cursos = (curso *) malloc(sizeof(curso)*3);
+    disciplina *disciplinas = (disciplina *) malloc(sizeof(disciplina)* Tamanho);
+	aluno *alunos = (aluno *) malloc(sizeof(aluno)*Tamanho);
+	turma *turmas = (turma *) malloc(sizeof(turma)*Tamanho);
+	curso *cursos = (curso *) malloc(sizeof(curso)*Tamanho);
 
+	/** Accountants struct */
+    int intContCurso = 0,intContTurma = 0, intCodCurso = 0;
 
-	/*Menu do sistema*/
+	/** Menu system */
 	int controle = 9999,entrada,controle1 = 9999,entrada1;
 	while(controle != 0){
     	printf("Selecione uma mingw32-gcc.exeopcao do menu:\n");
@@ -90,14 +175,15 @@ void main(){
                 	scanf("%i", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                        	if(fnCadastraCurso(cursos)){
+                    	    intContCurso = fnCadastraCurso(cursos,intContCurso);
+                        	if(intContCurso > -1){
                                 printf("Salvo!");
                         	}else{
                         	    printf("Erro!");
                         	}
                         	break;
                     	case 2:
-                        	/*fnListaCurso();*/
+                        	fnListaCurso(cursos,intContCurso);
                         	break;
                     	case 0:
                         	controle1 = 0;
@@ -115,10 +201,17 @@ void main(){
                 	scanf("%d", &entrada1);
                 	switch(entrada1){
                     	case 1:
-                        	/*fnCadastraTurma();*/
+                        	intContTurma = fnCadastraTurma(turmas,intContTurma);
+                        	if(intContTurma > -1){
+                                printf("Salvo!");
+                        	}else{
+                        	    printf("Erro!");
+                        	}
                         	break;
                     	case 2:
-                        	/*fnListaTurma(0);*/
+                    	    printf("Digite o codigo do curso, das turma que deseja listar:\n");
+                            scanf("%d", &intCodCurso);
+                        	fnListaTurma(intCodCurso,turmas,intContTurma,cursos);
                         	break;
                     	case 0:
                         	controle1 = 0;
